8a47ea80c70af8113f86841b8cfd1e72e1958443
LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>
2024-04-23 14:16:21 +0700

    dlt: Hello World
    Signed-off-by: LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # intern_demo
+
+This is a repo for dlt intern demo code and dlt features
---
+++ Section_1_Simple_Hello_World_with_DLT/hello_dlt.c
@@ -0,0 +1,14 @@
+#include <dlt/dlt.h>
+DLT_DECLARE_CONTEXT(hello)
+
+int main() {
+    DLT_REGISTER_APP("HLLO", "An app to send Hello mesg");
+    DLT_REGISTER_CONTEXT(hello, "HLL1", "A context for Hello");
+
+    DLT_LOG(hello, DLT_LOG_INFO, DLT_CSTRING("Hello World"));
+
+    DLT_UNREGISTER_CONTEXT(hello);
+    DLT_UNREGISTER_APP();
+    return 0;
+}
+
