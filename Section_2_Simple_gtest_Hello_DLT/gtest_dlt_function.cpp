0b34ade27cb8cb87583e57c48b9bc75e66190049
LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>
2024-05-06 11:52:08 +0700

    gtest with simple function
    Signed-off-by: LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>

---
+++ Section_2_Simple_gtest_Hello_DLT/gtest_dlt_function.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "hello_dlt.h"
+}
+
+TEST(t_dlt_log_to_console, normal) {
+    int level = STDOUT_RETURN_OK;
+    const char* str = "Hello World!";
+
+    EXPECT_EQ(STDOUT_RETURN_OK, dlt_log_to_console(level, str));
+    
+    //level = STDOUT_RETURN_ERROR;
+    //EXPECT_EQ(STDOUT_RETURN_ERROR, dlt_log_string(context, loglevel, text));
+
+    //level = STDOUT_RETURN_INVALID;
+
+
+}
+
+//TEST(t_dlt_log_string, abnormal) {
+
+//}
+
+//TEST(t_dlt_log_string, null) {
+
+//}
+
+int main(int argc, char ** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
--- Section_2_Simple_gtest_Hello_DLT/hello_dlt.c
+++ Section_2_Simple_gtest_Hello_DLT/hello_dlt.c
@@ -12,14 +12,3 @@ int dlt_log_to_console(int log_level, const char *text) {
     return log_level;
 }    
 
-int main() {
-    DLT_REGISTER_APP("HLLO", "An app to send Hello mesg");
-    DLT_REGISTER_CONTEXT(hello, "HLL1", "A context for Hello");
-    dlt_log_to_console(STDOUT_RETURN_OK, "Hello World");
-    DLT_LOG(hello, DLT_LOG_INFO, DLT_CSTRING("Hello World"));
-
-    DLT_UNREGISTER_CONTEXT(hello);
-    DLT_UNREGISTER_APP();
-    return 0;
-}
-
